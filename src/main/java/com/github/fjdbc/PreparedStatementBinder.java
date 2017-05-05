f6312c1930fac2da2ee56038d2c43289d9d595f9
Ewan Lassudrie-Duchesne <ewan.lassudrieduchesne@atos.net>
2017-05-05 09:43:32 +0200

    remove useless utility methods

--- src/main/java/com/github/fjdbc/PreparedStatementBinder.java
+++ src/main/java/com/github/fjdbc/PreparedStatementBinder.java
@@ -2,7 +2,6 @@ package com.github.fjdbc;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Collection;
 
 import com.github.fjdbc.util.IntSequence;
 
@@ -11,28 +10,12 @@ import com.github.fjdbc.util.IntSequence;
  */
 @FunctionalInterface
 public interface PreparedStatementBinder {
-	void bind(PreparedStatement st, IntSequence paramIndex) throws SQLException;
-
-	public default void bind(PreparedStatement st) throws SQLException {
-		bind(st, new IntSequence(1));
-	}
+	void bind(PreparedStatement ps, IntSequence index) throws SQLException;
 
 	public static PreparedStatementBinder create(Object... params) {
 		return (st, index) -> {
 			for (final Object param : params) {
 				st.setObject(index.next(), param);
-			}
-		};
-	}
-
-	public static PreparedStatementBinder createBatch(Collection<? extends Collection<Object>> params) {
-		return (st, index) -> {
-			for (final Collection<Object> paramCollection : params) {
-				for (final Object param : paramCollection) {
-					st.setObject(index.next(), param);
-				}
-				st.addBatch();
-				index.reset();
 			}
 		};
 	}
