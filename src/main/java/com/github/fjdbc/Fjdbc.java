9c3eeeb9c68caa0bfbd3342e7ef1dc0c61be26e0
Ewan Lassudrie-Duchesne <ewan.lassudrieduchesne@atos.net>
2018-01-18 10:02:42 +0100

    refactor: add javadoc

--- src/main/java/com/github/fjdbc/Fjdbc.java
+++ src/main/java/com/github/fjdbc/Fjdbc.java
@@ -1,5 +1,6 @@
 package com.github.fjdbc;
 
+import java.sql.PreparedStatement;
 import java.util.Collection;
 
 import com.github.fjdbc.op.CompositeOperation;
@@ -16,9 +17,8 @@ public class Fjdbc {
 
 	/**
 	 * Create a facade to the FJDBC library.
-	 * 
 	 * @param connectionProvider
-	 *            The provider of {@link Connection} instances.
+	 *        The provider of {@link Connection} instances.
 	 */
 	public Fjdbc(ConnectionProvider connectionProvider) {
 		this.cnxProvider = connectionProvider;
@@ -28,14 +28,23 @@ public class Fjdbc {
 	 * Create a statement.
 	 * <p>
 	 * No actual connection is established with the database until the statement is executed.
-	 * 
 	 * @param sql
-	 *            The raw SQL string to be executed.
+	 *        The raw SQL string to be executed.
 	 */
 	public StatementOperation statement(String sql) {
 		return new StatementOperation(cnxProvider, sql);
 	}
 
+	/**
+	 * Create a statement.
+	 * <p>
+	 * No actual connection is established with the database until the statement is executed.
+	 * @param sql
+	 *        The raw SQL string to be executed.
+	 * @param binder
+	 *        The binder of {@link PreparedStatement} parameters, or {@code null} if this query does not have
+	 *        parameters.
+	 */
 	public StatementOperation statement(String sql, PreparedStatementBinder binder) {
 		return new StatementOperation(cnxProvider, sql, binder);
 	}
@@ -62,16 +71,27 @@ public class Fjdbc {
 	 * Create a new query.
 	 * <p>
 	 * No actual connection is established with the database until the statement is executed.
-	 * 
 	 * @param sql
-	 *            The raw SQL string. It should be a SELECT statement.
+	 *        The raw SQL string. It should be a SELECT statement.
 	 * @param extractor
-	 *            Extracts individual objects from a {@link ResultSet}.
+	 *        Extracts individual objects from a {@link ResultSet}.
 	 */
 	public <T> Query<T> query(String sql, ResultSetExtractor<T> extractor) {
 		return new Query<>(cnxProvider, sql, extractor);
 	}
 
+	/**
+	 * Create a new query.
+	 * <p>
+	 * No actual connection is established with the database until the statement is executed.
+	 * @param sql
+	 *        The raw SQL string. It should be a SELECT statement.
+	 * @param binder
+	 *        The binder of {@link PreparedStatement} parameters, or {@code null} if this query does not have
+	 *        parameters.
+	 * @param extractor
+	 *        Extracts individual objects from a {@link ResultSet}.
+	 */
 	public <T> Query<T> query(String sql, PreparedStatementBinder binder, ResultSetExtractor<T> extractor) {
 		return new Query<>(cnxProvider, sql, binder, extractor);
 	}
